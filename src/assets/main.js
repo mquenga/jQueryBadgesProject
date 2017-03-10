7ac6ef93ce163f5140cef71742c412ecb47924bc
mquenga <mquenga@hawaii.edu>
2017-03-10 10:54:49 -1000

    div or each

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,10 +3,20 @@ $(function() {
     url: 'https://www.codeschool.com/users/2987780.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+      addCourses('response', response.course.completed);
+
     }
   });
 
+  function addCourses(courses){
 
+    var badges = $('#badges');
 
+    courses.forEach(function(course){
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+    })
+  }
 });
