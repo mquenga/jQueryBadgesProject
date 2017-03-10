9eb3791099e6f95b38d1c78dee3325611817d4dd
mquenga <mquenga@hawaii.edu>
2017-03-10 10:26:18 -1000

    added  ajax function with link to codeschool report card

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2987780.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
-  // your code will go here
+
 
 });
